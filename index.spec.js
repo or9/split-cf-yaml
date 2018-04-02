813f4a431d4cadd62d7a821181805f65d10fe5f9
Abdur Rahman <abdurrahman.abdullahalbasir@ge.com>
2018-04-02 12:16:15 -0400

    update tests

--- index.spec.js
+++ index.spec.js
@@ -1,1 +1,30 @@
 #!/usr/bin/env node
+
+const sinon = require("sinon");
+require("chai").should();
+
+describe("split-cf-yaml", () => {
+	it("should read the manifest.yml file", () => {
+
+	})
+
+	it("should write a new manifest.yml for each app listed", () => {
+
+	});
+
+	it("should accept an input_directory parameter", () => {
+
+	})
+
+	it("should accept an output_directory parameter", () => {
+
+	})
+
+	it("should read manifest.yml from provided input directory", () => {
+
+	})
+
+	it("should write manifests to provided output directory", () => {
+		
+	})
+});
